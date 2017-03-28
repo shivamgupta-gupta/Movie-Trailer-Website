a005eb0f61dace462f535b73c6f6eae51655fe3f
Shivam Gupta <shivamgupta.gupta1996@gmail.com>
2017-03-28 09:48:47 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,16 +1,24 @@
 import webbrowser
 
-""" Movie class consist of a constructor i.e __init__() which takes title, movie_quote, poster_image_url,trailer_youtube_url as an argument and a function
-    known as play_trailer which plays the youtube trailer for the movie. """
+""" Movie class consist of a constructor i.e __init__() which takes title,
+    movie_quote, poster_image_url,trailer_youtube_url as an argument."""
+
+""" It also contains a function known as play_trailer which plays the youtube
+    trailer for the movie. """
+
 
 class Movie:
-    def __init__(self, title, movie_quote, poster_image_url,trailer_youtube_url):
+    # constructor
+    def __init__(self,
+                 title,
+                 movie_quote,
+                 poster_image_url,
+                 trailer_youtube_url):
         self.title = title
         self.movie_quote = movie_quote
         self.trailer_youtube_url = trailer_youtube_url
         self.poster_image_url = poster_image_url
 
+    # function to play movie trailer
     def play_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-
-
